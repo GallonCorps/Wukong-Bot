ed3f7ec0b308b1fc5d39c8a157d8cb9721527f0e
MrCacaja <carlosprezzi@hotmail.com>
2020-08-13 12:29:58 -0300

    argumentos

--- commands/scramble.js
+++ commands/scramble.js
@@ -1,9 +1,14 @@
 function scramble(msg, args){
     var phrase = ""
-    args.forEach((arg) => {
-        const word = args[Math.floor(Math.random() * args.length)]
-        phrase = phrase + " " + word;
-    });
-    msg.channel.send(phrase);
+    if (args.length){
+        args.forEach((arg) => {
+            const word = args[Math.floor(Math.random() * args.length)]
+            phrase = phrase + " " + word;
+        });
+        msg.channel.send(phrase);
+    }
+    else {
+        msg.channel.send("onde estas argumento");
+    }
 }
 export {scramble}

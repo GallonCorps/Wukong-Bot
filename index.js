b197f331741f205e43278fb0dc1e4b0252389232
MrCacaja <carlosprezzi@hotmail.com>
2020-08-12 22:11:09 -0300

    less shitty code

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-import { Client } from 'discord.js';
-import {} from 'dotenv/config.js';
-import { isNumber } from 'util';
+import { Client } from "discord.js";
+import {} from "dotenv/config.js";
 
 const client = new Client();
 const prefix = process.env.BOT_PREFIX;
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content.startsWith(prefix)){
-        const args = msg.content.slice(prefix.length).trim().split(' ');
-        const command = args[0];
-        switch(command){
-            case("spam"):
-                args.forEach((arg) => {
-                    if (arg != command){
-                        msg.channel.send(arg);
-                    }
-                })
-            case("sum"):
-                const number = Number(args[1]) + Number(args[2]);
-                if (isNaN(number)){
-                    msg.channel.send("alguma coisa ai não é número burro")
-                } else {
-                    msg.channel.send(`A soma é: ${number}`)
-                }
+client.on("message", (msg) => {
+  if (msg.content.startsWith(prefix)) {
+    const args = msg.content.slice(prefix.length).trim().split(" ");
+    const command = args[0];
+    args.shift();
+    switch (command) {
+      case "spam":
+        args.forEach((arg) => {
+          if (arg != command) {
+            msg.channel.send(arg);
+          }
+        });
+        break;
+      case "sum":
+        const number = Number(args[0]) + Number(args[1]);
+        if (isNaN(number)) {
+          msg.channel.send("alguma coisa ai não é número burro");
+        } else {
+          msg.channel.send(`A soma é: ${number}`);
         }
+        break;
     }
+  }
 });
 
 client.login(process.env.BOT_TOKEN);

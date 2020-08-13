a446310cfc04a48439e7e52b2ab892e1b4817c1f
borgfoda <brunovoador3@outlook.com>
2020-08-12 21:06:56 -0300

    dotenv e type module

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('dotenv').config()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content == 'ping') {
     msg.reply('pong');
   }
 });
 
-client.login('NzE3NTE4Nzg3MjgwMjQwNjkw.XtbfcQ.z6wPsI3mjPQoR-UrF0jpjvJ42nc');
+client.login(process.env.BOT_TOKEN);
